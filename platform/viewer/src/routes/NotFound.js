79f6c12d67ab2b229685c9f8faf1fcf0d2bd37ae
NatiTm <wedenati@gmail.com>
2020-03-02 17:13:42 +0300

    not found page modified to point to /hms/

--- platform/viewer/src/routes/NotFound.js
+++ platform/viewer/src/routes/NotFound.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './NotFound.css';
 import { Link } from 'react-router-dom';
 
-export default function NotFound({ message = 'Sorry, this page does not exist.', showGoBackButton = true }) {
+export default function NotFound({
+  message = 'Sorry, this page does not exist.',
+  showGoBackButton = true,
+}) {
   return (
     <div className={'not-found'}>
       <div>
         <h4>{message}</h4>
         {showGoBackButton && (
           <h5>
-            <Link to={'/'}>Go back to the Study List</Link>
+            <Link to={'/hms/'}>Go back to dashboard</Link>
           </h5>
         )}
       </div>
